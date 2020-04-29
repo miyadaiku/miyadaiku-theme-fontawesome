e1f325cdb1849023c35f34fc22df33863db83957
Atsuo Ishimoto <atsuoishimoto@users.noreply.github.com>
2020-04-30 01:10:21 +0900

    reafactoring (#1)
    miyadaiku 1.0.0

---
+++ miyadaiku_theme_fontawesome/__init__.py
@@ -0,0 +1,21 @@
+import pkg_resources
+from miyadaiku import config
+
+__version__ = '1.0.0'
+
+FONTAWESOME_MIN = 'all.min.js'
+FONTAWESOME = 'all.js'
+DEST_PATH = '/static/fontawesome/js/'
+
+def load_package(site):
+    f = site.config.get('/', 'fontawesome_compressed')
+    f = config.to_bool(f)
+    fontawesome = FONTAWESOME_MIN if f else FONTAWESOME
+    src_path = 'externals/js/'+fontawesome
+    
+    content = pkg_resources.resource_string(__name__, src_path)
+    site.files.add_bytes("binary", DEST_PATH + fontawesome , content )
+    site.config.add('/', {'fontawesome_path': DEST_PATH+fontawesome})
+
+    site.add_template_module('fontawesome', 'miyadaiku_theme_fontawesome!macros.html')
+
